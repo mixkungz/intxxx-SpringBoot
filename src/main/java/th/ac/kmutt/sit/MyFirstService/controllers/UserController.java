429be506590c67586e35278b7c7b75d88a2f6bfc
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-08-25 17:04:12 +0700

    :tada::sparkle: Init project with Spring Boot & Create route /users and /users/{id}.

---
+++ src/main/java/th/ac/kmutt/sit/MyFirstService/controllers/UserController.java
@@ -0,0 +1,31 @@
+package th.ac.kmutt.sit.MyFirstService.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable("id") int id) {
+        ArrayList<User> users = new ArrayList();
+        users.add(new User(1,"A"));
+        users.add(new User(2,"B"));
+        users.add(new User(3,"C"));
+        users.add(new User(4,"D"));
+        return users.get(id-1);
+    }
+
+    @GetMapping("/users")
+    public ArrayList<User> getUser() {
+        ArrayList users = new ArrayList();
+        users.add(new User(1,"A"));
+        users.add(new User(2,"B"));
+        users.add(new User(3,"C"));
+        users.add(new User(4,"D"));
+        return users;
+    }
+}
