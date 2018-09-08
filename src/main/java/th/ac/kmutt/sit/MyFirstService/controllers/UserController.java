e6fa0d4f998d7d53a2ba4317c494c7b06c32665b
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-09-08 13:46:45 +0700

    :recycle: Refactor use constructure to create user.

--- src/main/java/th/ac/kmutt/sit/MyFirstService/controllers/UserController.java
+++ src/main/java/th/ac/kmutt/sit/MyFirstService/controllers/UserController.java
@@ -8,27 +8,25 @@ import java.util.ArrayList;
 
 @RestController
 public class UserController {
+    ArrayList<User> users;
 
-    @GetMapping("/users/{id}")
-    public User getUser(@PathVariable("id") int id) {
-        ArrayList<User> users = new ArrayList();
+    public UserController() {
+        users = new ArrayList();
         users.add(new User(1,"A"));
         users.add(new User(2,"B"));
         users.add(new User(3,"C"));
         users.add(new User(4,"D"));
         users.add(new User(5,"E"));
 
-        return users.get(id-1);
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable("id") int id) {
+        return this.users.get(id-1);
     }
 
     @GetMapping("/users")
     public ArrayList<User> getUser() {
-        ArrayList users = new ArrayList();
-        users.add(new User(1,"A"));
-        users.add(new User(2,"B"));
-        users.add(new User(3,"C"));
-        users.add(new User(4,"D"));
-        users.add(new User(5,"E"));
-        return users;
+        return this.users;
     }
 }
